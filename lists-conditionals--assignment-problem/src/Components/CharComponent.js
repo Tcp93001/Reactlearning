d16e9184c7723b49d8174e60c3eb576d1ecca9db
Tcp93001 <joseguerreroroman@yahoo.com.mx>
2018-11-07 18:53:58 -0600

    small change

--- lists-conditionals--assignment-problem/src/Components/CharComponent.js
+++ lists-conditionals--assignment-problem/src/Components/CharComponent.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const CharComponent = (props) => {
-
+// TO DO include the prop.inputString and convert it into an array, then change again
+//you will need a prop.function for returning the click on each block
 };
 
 export default CharComponent;
